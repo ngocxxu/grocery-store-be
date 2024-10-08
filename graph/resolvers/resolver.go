db0126d0bc7e973aba09a1016c2dfa608f50bfbb
bono43 <bono43@nexondv.com>
2024-10-08 16:37:43 +0700

    feat: add utils

--- graph/resolvers/resolver.go
+++ graph/resolvers/resolver.go
@@ -7,9 +7,13 @@ import (
 
 	"github.com/ngocxxu/grocery-store-svelte-be/graph/generated"
 	"github.com/ngocxxu/grocery-store-svelte-be/graph/model"
+	"github.com/ngocxxu/grocery-store-svelte-be/internal/service"
 )
 
-type Resolver struct{}
+type Resolver struct {
+	UserService    *service.UserService
+	ProductService *service.ProductService
+}
 
 // CreateUser is the resolver for the createUser field.
 func (r *mutationResolver) CreateUser(ctx context.Context, name string, email string) (*model.User, error) {
@@ -49,16 +53,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	type Resolver struct{
-    UserService *service.UserService
-    ProductService *service.ProductService
-}
-*/
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,10 +1,25 @@
 package utils
 
 import (
-    "regexp"
+	"regexp"
+	"strconv"
+
+	graphModel "github.com/ngocxxu/grocery-store-svelte-be/graph/model"
+	internalModel "github.com/ngocxxu/grocery-store-svelte-be/internal/model"
 )
 
 func IsValidEmail(email string) bool {
-    emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
-    return emailRegex.MatchString(email)
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(email)
+}
+
+func ConvertToGraphWeightOptions(internalWeightOptions []internalModel.WeightOption) []*graphModel.WeightOption {
+	graphWeightOptions := make([]*graphModel.WeightOption, len(internalWeightOptions))
+	for i, opt := range internalWeightOptions {
+		graphWeightOptions[i] = &graphModel.WeightOption{
+			Weight: opt.Weight,
+			ID:     strconv.FormatUint(uint64(opt.UnitID), 10),
+		}
+	}
+	return graphWeightOptions
 }
